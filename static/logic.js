c2560b15b4554d1951da48835de6384eebb0bcc5
Alexander Lecocq <alexander.m.lecocq@gmail.com>
2019-06-24 21:59:59 -0500

    leaflet compiles but does not display

--- static/logic.js
+++ static/logic.js
@@ -31,20 +31,19 @@ function createMap(petFind) {
     }).addTo(map);
 };
   
-function createMarkers(response) {
+function createMarkers(pets) {
     // Pull the "stations" property off of response.data
-    var stations = response.data.stations;
 
     // Initialize an array to hold pet markers
     var petMarkers = [];
 
     // Loop through the stations array
-    for (var index = 0; index < stations.length; index++) {
-        var station = stations[index];
+    for (var index = 0; index < pets.length; index++) {
+        var pet = pets[index];
 
         // For each station, create a marker and bind a popup with the station's name
-        var petMarker = L.marker([station.lat, station.lon])
-        .bindPopup("<h3>" + station.name + "<h3><h3>Capacity: " + station.capacity + "<h3>");
+        var petMarker = L.marker([pet.coordinates[0], pet.coordinates[1]])
+        .bindPopup("<h3>" + pet.title + "<h3>");
 
         // Add the marker to the petMarkers array
         petMarkers.push(petMarker);
